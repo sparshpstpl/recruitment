54181337ce6f0b23ae946e1da36f0f85d09b9fbb
sparshpstpl <sparshpstpl@gmail.com>
2018-11-26 20:27:20 +0530

    added more fields on properties

---
+++ api/src/schema/PropertyInputType.js
@@ -0,0 +1,15 @@
+import { GraphQLID, GraphQLInputObjectType, GraphQLFloat, GraphQLInt } from 'graphql';
+
+export const fields = {
+  id: { type: GraphQLID },
+  livingSurface: { type: GraphQLFloat },
+  landSurface: { type: GraphQLFloat },
+  numberOfRooms: { type: GraphQLFloat },
+  numberOfParkings: { type: GraphQLInt },
+};
+
+export const PropertyInputType = new GraphQLInputObjectType({
+  name: 'PropertyInput',
+  fields,
+});
+
---
+++ api/src/schema/PropertyType.js
@@ -0,0 +1,46 @@
+/* @flow */
+
+import { GraphQLObjectType, GraphQLFloat, GraphQLInt } from 'graphql';
+import { GraphQLDateTime } from 'graphql-iso-date';
+
+import { globalIdField } from 'graphql-relay';
+import { nodeInterface } from './node';
+
+export const PropertyType = new GraphQLObjectType({
+  name: 'Property',
+  interfaces: () => [nodeInterface],
+  fields: () => ({
+    id: globalIdField(),
+
+    livingSurface: {
+      type: GraphQLFloat,
+      resolve: parent => parent.living_surface,
+    },
+
+    landSurface: {
+      type: GraphQLFloat,
+      resolve: parent => parent.land_surface,
+    },
+    
+    numberOfRooms: {
+      type: GraphQLFloat,
+      resolve: parent => parent.number_of_rooms,
+    },
+    
+    numberOfParkings: {
+      type: GraphQLInt,
+      resolve: parent => parent.number_of_parkings,
+    },
+
+    createdAt: {
+      type: GraphQLDateTime,
+      resolve: parent => parent.created_at,
+    },
+
+    updatedAt: {
+      type: GraphQLDateTime,
+      resolve: parent => parent.updated_at,
+    },
+  }),
+});
+
